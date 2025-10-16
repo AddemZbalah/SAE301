042848a5beaf9cb33cf773cf9e1981bb1a743384
Zbalah Addem <addem.zbalah@etu.unilim.fr>
2025-10-16 22:56:48 +0200

    US1 fonctionnel

--- client/src/ui/product/index.js
+++ client/src/ui/product/index.js
@@ -3,7 +3,8 @@ import template from "./template.html?raw";
 
 let ProductView = {
   html: function (data) {
-    let htmlString = '<div style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 1.5rem;">';
+     let htmlString =
+      '<div class="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4 px-4 md:px-6 lg:px-8">';
     for (let obj of data) {
       htmlString  += genericRenderer(template, obj);
     }
