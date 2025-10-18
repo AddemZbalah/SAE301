964aa89b3cfa12b05637ff5916f7a23bedcb95ae
Zbalah Addem <addem.zbalah@etu.unilim.fr>
2025-10-18 17:53:36 +0200

    US2 terminé et  les problèmes reglés

--- client/src/ui/product/index.js
+++ client/src/ui/product/index.js
@@ -4,7 +4,7 @@ import template from "./template.html?raw";
 let ProductView = {
   html: function (data) {
      let htmlString =
-      '<div class="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4 px-4 md:px-6 lg:px-8">';
+      '<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-8 md:px-12 lg:px-16 py-12">';
     for (let obj of data) {
       htmlString  += genericRenderer(template, obj);
     }
