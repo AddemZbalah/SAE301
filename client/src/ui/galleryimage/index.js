a3c9ed270b23640a6a542bdfcb7ff65adcb40fc0
Zbalah Addem <addem.zbalah@etu.unilim.fr>
2025-10-20 23:50:17 +0200

    US3 terminé il manque juste responsive tablette bug a regler

--- client/src/ui/galleryimage/index.js
+++ client/src/ui/galleryimage/index.js
@@ -4,17 +4,17 @@ export default function renderImageGalerie({ images }) {
 
   const mainImage = document.createElement("img");
   mainImage.src = `/assets/image/products/${images[0]}`;
-  mainImage.className = "w-full h-full object-cover rounded";
+  mainImage.className = "object-cover";
   container.appendChild(mainImage);
 
   const gallery = document.createElement("div");
-  gallery.className = "flex flex-col absolute top-40 space-x-2 mt-4";
+  gallery.className = "flex gap-2 flex-col absolute top-40 space-x-2 mt-4";
 
   images.forEach((img, i) => {
     const thumb = document.createElement("img");
     thumb.src = `/assets/image/products/${img}`;
     thumb.alt = `Miniature ${i + 1}`;
-    thumb.className = "flex flex-col w-16 h-16 object-contain rounded cursor-pointer border-2 border-transparent";
+    thumb.className = "flex flex-col w-16 h-16 object-contain cursor-pointer border-1 border-b";
 
     thumb.onclick = () => {
       mainImage.src = `/assets/image/products/${img}`;
--- client/src/ui/product/index.js
+++ client/src/ui/product/index.js
@@ -4,7 +4,7 @@ import template from "./template.html?raw";
 let ProductView = {
   html: function (data) {
      let htmlString =
-      '<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-8 md:px-12 lg:px-16 py-12">';
+      '<div class="grid grid-cols-1 place-content-center md:grid-cols-2 lg:grid-cols-3 gap-x-15 gap-y-15 px-8 md:px-12 gap-x-30 lg:px-16 ">';
     for (let obj of data) {
       let image = `/assets/image/products/${obj.images[0]}`;
       obj.image = image;
